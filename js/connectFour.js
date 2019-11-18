3aa2e3bf189822f301b6c6caa89d23dd0d305189
csundbeck <csundbeck04@gmail.com>
2019-11-17 19:19:43 -0600

    Finished checkpoint 3 - connect four game

--- js/connectFour.js
+++ js/connectFour.js
@@ -3,27 +3,28 @@ var btnRestart = document.getElementById('restartGame');
 
 $(document).ready(function() {
     let color = 'black';
+    //Function to add game pieces to a column when the specific column is clicked
     $('.column').click(function() {
-      $(this).append(`<div class="disc ${color}"></div>`);
-      if (color == 'red') {
-        color = 'black';
-        $("#turn").css("background-color", "black")
-      } else {
-        color = 'red';
-        $("#turn").css("background-color", "red")
-      }
-      for (let i=0; i < boardColumn.length; i++) {
-        if (boardColumn[i].childElementCount < 7) {
-            continue;
-        } else if (boardColumn[i].childElementCount == 7) {
-            break;
-        }
+      if (this.childElementCount < 7) {
+        $(this).append(`<div class="${color}"></div>`);
+          //On click, change the coor after the game piece  placed
+          if (color == 'red') {
+            color = 'black';
+            $("#turn").css("background-color", "black")
+          } else {
+            color = 'red';
+            $("#turn").css("background-color", "red")
+          }
+    //Stop adding child nodes/game pieces if the number in a column reaches 7
+    } else if (this.childElementCount >= 7) {
+      $(this).click(function(event) {
+        event.stopImmediatePropagation();
+      });
     }
     });
   });
 
-
-
+  //Remove game pieces from the board to reset the game.
   function restartGame() {
     for (let i=0; i < boardColumn.length; i++) {
         boardColumn[i].innerHTML = '';
